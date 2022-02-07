fc6b25b3cb7fe891278e74eeb3757896eac00545
dfr13 <danielferrerroman0@gmail.com>
2022-02-07 14:13:21 +0100

    Terminado Ejer1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import './style.css';
-import Navbar from './components/Navbar';
-import Ejercicio1 from './components/Ejercicio1';
-import Ejercicio2 from './components/Ejercicio2';
-import Ejercicio3 from './components/Ejercicio3';
+import Header from './components/Header';
+import { MenuElements } from './data/MenuElements';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 export default function App() {
   return (
-    <>
-      <Navbar />
-      <h1>Desarrollo de Interfaces: Examen React.js </h1>
-      <p>
-        Resuelve los ejercicios propuestos en los distintos apartados de la
-        barra de navegación. Para ello, haz un fork de este repositorio desde tu
-        cuenta de Github y súbelo a la tarea de examen. El repositorio debe
-        tener el nombre examen-react-Nombre-Apellido1, donde nombre y apellido
-        son los tuyos.
-      </p>
-      <Ejercicio1 />
-      <Ejercicio2 />
-      <Ejercicio3 />
-    </>
+    <Router>
+    <Header />
+    <h1>Desarrollo de Interfaces: Examen React.js </h1>
+    <p>
+      Resuelve los ejercicios propuestos en los distintos apartados de la
+      barra de navegación. Para ello, haz un fork de este repositorio desde tu
+      cuenta de Github y súbelo a la tarea de examen. El repositorio debe
+      tener el nombre examen-react-Nombre-Apellido1, donde nombre y apellido
+      son los tuyos.
+    </p>
+    {MenuElements.map((item) => {
+      return (
+        <Route
+          key={item.id}
+          path={item.path}
+          exact
+          component={item.component}
+        />
+      );
+    })}
+    
+  </Router>
   );
 }
---
+++ src/data/MenuElements.js
@@ -0,0 +1,24 @@
+import Ejercicio1 from '../components/Ejercicio1';
+import Ejercicio2 from '../components/Ejercicio2';
+import Ejercicio3 from '../components/Ejercicio3';
+
+export const MenuElements = [
+  {
+    id: 1,
+    path: '/ejercicio1',
+    title: 'Ejercicio 1',
+    component: Ejercicio1,
+  },
+  {
+    id: 2,
+    path: '/ejercicio2',
+    title: 'Ejercicio 2',
+    component: Ejercicio2,
+  },
+  {
+    id: 3,
+    path: '/ejercicio3',
+    title: 'Ejercicio 3',
+    component: Ejercicio3,
+  },
+];
